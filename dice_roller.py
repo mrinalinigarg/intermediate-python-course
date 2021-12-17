cf7b1d080d90e93f473f2619c95f0968e22240e4
Mrinalini <mrinalini2010@gmail.com>
2021-12-17 11:36:19 -0600

    Critical die rolls

--- dice_roller.py
+++ dice_roller.py
@@ -5,8 +5,16 @@ def main():
   sum=0
   for i in range(0, dice_rolls):
     roll=random.randint(1,6)
-    print(f'You rolled a {roll}')
+    if(roll==1):
+      print(f'You rolled a {roll}! Critical failure')
+    elif roll == 6:
+      print(f'You rolled a {roll}! Critical Success!')
+    else:
+      print(f'You rolled a {roll}')
+    
     sum+=roll
+
+
   print(f'You have rolled a total of {sum}')
 
 if __name__== "__main__":
